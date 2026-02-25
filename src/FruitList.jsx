86c978bffbcb93bcdbaef7634d4ecb9f4d3d8df3
Shewak-Sewani <shewaksewani@gmail.com>
2026-02-25 00:44:32 -0800

    first commit

---
+++ src/FruitList.jsx
@@ -0,0 +1,14 @@
+function FruitList({ fruits }) {
+  return (
+    <div>
+      <h2>Fruit List</h2>
+      <ul>
+        {fruits.map((fruit, index) => (
+          <li key={index}>{fruit}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default FruitList;
